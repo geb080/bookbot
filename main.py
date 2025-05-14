1b4e4779cad71b7518ddab34a863ba58185e02f2
geb080 <gabriel.bailey71@gmail.com>
2025-05-13 20:26:35 -0500

    C: just adding progress so far

--- main.py
+++ main.py
@@ -1,22 +1,10 @@
-path_to_file = "books/frankenstein.txt"
-
+from stats import count_words
+from stats import character_counter
 
 def main():
     num_words = count_words()
+    character_count = character_counter()
     print(f"{num_words} words found in the document")
+    print(character_count)
 
-
-def get_book_text(x):
-    with open(x) as open_the_book:
-        open_book = open_the_book.read()
-    return open_book
-
-def count_words():
-    words_to_split = get_book_text(path_to_file)
-    split_words = words_to_split.split()
-    num_of_words = len(split_words)
-    return num_of_words
-
-
-#count_words()
 main()
---
+++ stats.py
@@ -0,0 +1,26 @@
+path_to_file = "books/frankenstein.txt"
+
+
+def get_book_text(x):
+    with open(x) as open_the_book:
+        open_book = open_the_book.read()
+    return open_book
+
+words_to_split = get_book_text(path_to_file)
+
+def count_words():  
+    split_words = words_to_split.split()
+    num_of_words = len(split_words)
+    return num_of_words
+
+def character_counter():
+    character_dict = {}
+    character_list = []
+    for character in words_to_split:
+        if character not in character_list:
+            new_char = character.lower()
+            character_list.append(new_char)
+        else:
+            continue
+        print(character_list)
+    return new_char
